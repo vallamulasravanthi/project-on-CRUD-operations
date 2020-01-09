c8595101f63f8e5310309be98797a184222cefc9
vallamulasravanthi <59274092+vallamulasravanthi@users.noreply.github.com>
2020-01-09 04:22:49 -0800

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,51 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class DatabaseConnection {
+   
+    public static Connection getDatabaseConnection()
+    {
+        Connection con=null;
+     try {
+            //step1 load the driver class
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+//step2 create  the connection object
+
+        try {
+            con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+            System.out.println("Connection established");
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    
+    return con;
+    
+    }
+     public static void main(String args[])
+     {
+     
+     
+     
+     
+     }
+}
